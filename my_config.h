b2ecc78a6b0cbabcf2a06006e677233496f5d168
shironats <66661623+shironats@users.noreply.github.com>
2021-12-02 14:57:27 +0700

    Update my_config.h

--- my_config.h
+++ my_config.h
@@ -5,8 +5,6 @@
 #include <MFRC522.h>        // for RFID (MFRC522 by GithubCommunity (v.1.4.7))
 #include <SPI.h>            // for RFID and RTC
 #include <Ethernet.h>       // for ethernet shield (Ethernet by VARIOUS (v.2.0.0))
-#include <EthernetUdp.h>    // for RTC
-#include <TimeLib.h>        // for RTC (Time by Michael Margolis (v.1.6.0))
 
 //================ Important variables ==============
 #define NO_OF_CARDS 3
@@ -34,14 +32,6 @@ EthernetClient client;
 char server[] = "api.pushingbox.com";
 bool uploadStatus = false;
 
-// Online RTC
-#define TIME_ZONE 7                         // Waktu Indonesia Barat
-#define NTP_PACKET_SIZE 48                  // NTP time stamp is in the first 48 bytes of the message
-const char timeServer[] = "time.nist.gov";  // time.nist.gov NTP server
-byte packetBuffer[NTP_PACKET_SIZE];         //buffer to hold incoming and outgoing packets
-EthernetUDP Udp;
-unsigned int localPort = 8888;              // local port to listen for UDP packets
-
 //RFID
 #define CS_RFID 4   //pin 4
 #define RST_RFID 9  //pin 9
@@ -51,11 +41,5 @@ String IDName;
 int poi = 99;   // Person Of Interest (used in verifyUserCheckin)
 int tempId[4] = {0, 0, 0, 0};
 bool checkInStatus[NO_OF_CARDS] = {false, false, false};
-
-// time information
-#define CHECK_IN_HOUR 9
-#define CHECK_IN_MINUTE 5
-int userCheckInHour;
-int userCheckInMinute;
 
 #endif
